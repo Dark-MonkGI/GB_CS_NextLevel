f679e09af849996149d4f4275f819946f091e92d
Dark-MonkGI <gryaznov774@gmail.com>
2023-02-02 17:59:04 +0400

    ctreated new project

--- Asteroid/Program.cs
+++ Asteroid/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Asteroid
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroid
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
